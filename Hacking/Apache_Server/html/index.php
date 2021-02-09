6dcbbae364a7849de24ab53530c1c924c4eecfc6
Hugo <hugolachkar@gmail.com>
2021-02-09 15:29:00 +0100

    [ADD] ex03 hack

--- Hacking/Apache_Server/html/index.php
+++ Hacking/Apache_Server/html/index.php
@@ -1,26 +1,33 @@
-<?php
-session_start();
-$errorMsg = "";
-if(isset($_POST["sub"])) {
-  $validUser = $_POST["username"] == "admin" && $_POST["password"] == "password";
-  if(!$validUser) $errorMsg = "Invalid username or password.";
-}
-if($validUser) {
-   header("Location: /success.php"); die();
-}
-?>
 <!DOCTYPE html>
 <html>
+<script>
+var url_string = window.location.href
+var url = new URL(url_string);
+var username = url.searchParams.get("usrname");
+var password = url.searchParams.get("psw");
+if (username && password) {
+if (username == "admin" && password == "123")
+    alert("Success Bro");
+else
+    alert("Nope!");
+}
+</script>
 <head>
   <meta http-equiv="content-type" content="text/html;charset=utf-8" />
   <title>Login</title>
 </head>
+
 <body>
-  <form name="input" action="" method="post">
-    <label for="username">Username:</label><input type="text" value="<?= $_POST["username"] ?>" id="username" name="username" /><br />
-    <label for="password">Password:</label><input type="password" value="" id="password" name="password" /><br />
-    <div class="error"><?= $errorMsg ?></div><br />
-    <input type="submit" value="Home" name="sub" /><br />
-  </form>
+  <div class="container">
+    <form action="/">
+      <label for="usrname">Username</label>
+      <input type="text" id="usrname" name="usrname" required>
+
+      <label for="psw">Password</label>
+      <input type="password" id="psw" name="psw"  required>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
 </body>
+
 </html>
